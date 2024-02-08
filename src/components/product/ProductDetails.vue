577f48740cdae53d8ac6ef70dbafa9e46c5f43a7
Mohsen Piri <piri.mohsen@ut.ac.ir>
2024-02-08 23:03:05 +0330

    ✨ create product card component

---
+++ src/components/product/ProductDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="mt-10 flex flex-col items-start justify-center space-y-4 py-8 px-4 sm:flex-row sm:space-y-0 md:justify-between lg:px-0"
+  >
+    <div class="max-w-lg">
+      <h1 class="text-2xl font-bold text-gray-800">کتاب‌های کنکور</h1>
+      <p class="mt-2 text-gray-600">
+        در این صفحه می‌توانید پس از مشاهده کتاب ها . کتاب مورد علاقه خود را به سبد خرید اضافه کرده و
+        آنها را خریداری کنید.
+      </p>
+    </div>
+    <div class="">۱۲۰</div>
+  </div>
+</template>
