bddeba5fe7c74df27c48f03cdb557a1bab76c021
Mohsen Piri <piri.mohsen@ut.ac.ir>
2024-02-08 19:20:17 +0330

    💄 change css style for direction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,10 @@
       class="mt-10 flex flex-col items-start justify-center space-y-4 py-8 px-4 sm:flex-row sm:space-y-0 md:justify-between lg:px-0"
     >
       <div class="max-w-lg">
-        <h1 class="text-2xl font-bold text-gray-800">Beep Essences</h1>
+        <h1 class="text-2xl font-bold text-gray-800">کتاب‌های کنکور</h1>
         <p class="mt-2 text-gray-600">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, alias. Quas
-          necessitatibus exercitationem praesentium.
+          در این صفحه می‌توانید پس از مشاهده کتاب ها . کتاب مورد علاقه خود را به سبد خرید اضافه کرده
+          و آنها را خریداری کنید.
         </p>
       </div>
       <div class="">
